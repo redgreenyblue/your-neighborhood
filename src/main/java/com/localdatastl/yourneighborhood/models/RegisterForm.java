d8aaaa5cb3610a2552077bb486e1c910042b6e49
Rodney Halbert <rodneyhalbert@rocketmail.com>
2020-08-24 18:53:30 -0500

    initial commit

--- src/main/java/com/localdatastl/yourneighborhood/models/RegisterForm.java
+++ src/main/java/com/localdatastl/yourneighborhood/models/RegisterForm.java
@@ -1,4 +1,14 @@
 package com.localdatastl.yourneighborhood.models;
 
-public class RegisterForm {
+public class RegisterForm extends LoginForm {
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
 }
